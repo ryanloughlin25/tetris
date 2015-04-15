587034f8db50bea0d0950517a043a3654e017af6
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-04-15 11:17:46 -0700

    render first tetromino

--- js/game_board.js
+++ js/game_board.js
@@ -10,6 +10,30 @@ Game.prototype.render = function() {
   this.view.render(this.model);
 };
 
+Game.prototype.bindEvents = function() {
+  //TODO: move pressKey
+  var pressKey = function(event) {
+    event.preventDefault();
+    switch(event.which) {
+      case 37:
+        break;
+      case 38:
+        break;
+      case 39:
+        break;
+      case 40:
+        break;
+
+      default: return;
+    }
+  };
+  $(document).on('keydown', pressKey);
+};
+
+Game.prototype.move = function() {
+  return;
+};
+
 Game.Model = function() {
   this.height = 20;
   this.width = 10;
@@ -17,6 +41,7 @@ Game.Model = function() {
   for (var i = 0; i < this.height; i++) {
     this.board[i] = new Array(this.width);
   };
+  this.active_tetromino = new Tetromino();
 };
 
 Game.View = function() {};
@@ -34,10 +59,43 @@ Game.View.prototype.render = function(model) {
     }
     $('.game_board').append(row);
   }
+  //render tetromino
+  var tetromino = model.active_tetromino;
+  for(var i = tetromino.coords.x; i < tetromino.size; i++) {
+    var row = $('.row:eq(' + i + ')');
+    for(var j = tetromino.coords.y; j < tetromino.size; j++) {
+      var col = row.children()[j];
+      if (tetromino.occupies(i, j)) {
+        col.style.backgroundColor = tetromino.color;
+      }
+    }
+  }
+};
+
+Tetromino = function() {
+  this.type = 'T';
+  this.color = 'red';
+  this.size = 3;
+  this.coords = {
+    x: 0,
+    y: 0,
+  }
+  this.bounding_square = new Array(this.size);
+  for (var i = 0; i < this.size; i++) {
+    this.bounding_square[i] = new Array(this.size);
+  }
+  this.bounding_square[0][1] = true;
+  this.bounding_square[1][0] = true;
+  this.bounding_square[1][1] = true;
+  this.bounding_square[1][2] = true;
+  this.occupies = function(i, j) {
+    return this.bounding_square[i][j];
+  };
 };
 
 var setup = function() {
   game = new Game();
+  game.bindEvents();
   game.render();
 };
 
