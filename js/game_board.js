14a536859db9dcde8313df4c0f9abc318da443ff
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-04-15 08:48:43 -0700

    finish previous commit, refactor js to oo

--- js/game_board.js
+++ js/game_board.js
@@ -1,19 +1,35 @@
-var GameBoard = function() {
-  this.model = new Model();
-  this.view = new View();
+var Game = function() {
+  this.model = new Game.Model();
+  this.view = new Game.View();
 };
 
-var GameBoard.Model = function() {
+Game.prototype.render = function() {
+  console.log(this);
+  console.log(this.view);
+  console.log(this.model);
+  this.view.render(this.model);
+};
+
+Game.Model = function() {
   this.height = 20;
   this.width = 10;
-  this.board = 
+  this.board = new Array(this.height);
+  for (var i = 0; i < this.height; i++) {
+    this.board[i] = new Array(this.width);
+  };
 };
 
-var buildBoard = function() {
-  for(var i = 0; i < height; i++) {
+Game.View = function() {};
+
+Game.View.prototype.render = function(model) {
+  for(var i = 0; i < model.height; i++) {
     var row = $('<div class="row"></div>');
-    for(var j = 0; j < width; j++) {
+    for(var j = 0; j < model.width; j++) {
       var column = $('<div class="col"></div>');
+      //color squares
+      if (typeof model.board[i][j] != 'undefined') {
+        column.css("backgroundColor", model.board[i][j]);
+      }
       row.append(column);
     }
     $('.game_board').append(row);
@@ -21,7 +37,8 @@ var buildBoard = function() {
 };
 
 var setup = function() {
-  buildBoard();
+  game = new Game();
+  game.render();
 };
 
 $(document).ready(setup);
