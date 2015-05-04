b97493b36cb69c45a5ede106f09396ce1a0fce8e
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-05-04 10:51:14 -0700

    implement tetromino rotation.  create helpers file for matrix transpose function

---
+++ js/helpers.js
@@ -0,0 +1,9 @@
+var transpose = function(matrix) {
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < i; j++) {
+      var temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+};
